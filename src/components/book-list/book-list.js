3e3ffcc05bc0afa714df4cc22b50b68390fbeac4
MemboLembo <1nfinitiv6995@gmail.com>
2020-05-30 16:21:10 +0300

    refactoring book-list with ownProps

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import BookListItem from '../book-list-item';
 
 import { withBookstoreService } from '../hoc';
-import { booksLoaded, booksRequested, booksError } from '../../actions';
+import { fetchBooks } from '../../actions';
 import { compose } from '../../utils';
 import Spinner from '../spinner';
 import ErrorIndicator from '../error-indicator';
@@ -13,17 +13,7 @@ import './book-list.css';
 
 class BookList extends Component {
   componentDidMount() {
-    const {
-      bookstoreService,
-      booksLoaded,
-      booksRequested,
-      booksError,
-    } = this.props;
-
-    booksRequested();
-    bookstoreService.getBooks()
-      .then((data) => booksLoaded(data))
-      .catch((error) => booksError(error));
+    this.props.fetchBooks();
   }
 
   render() {
@@ -55,10 +45,10 @@ const mapStateToProps = ({ books, loading, error }) => {
   return { books, loading, error };
 };
 
-const mapDispatchToProps = {
-  booksLoaded,
-  booksRequested,
-  booksError,
+const mapDispatchToProps = (dispatch, { bookstoreService }) => {
+  return {
+    fetchBooks: fetchBooks(bookstoreService, dispatch),
+  };
 };
 
 export default compose(
